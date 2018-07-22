58b182bd0c67fd365842b106575a8c0bf3bc1b46
Unitedideas <unitedideas@gmail.com>
2018-07-22 08:38:25 -0700

    init gomoto for gcloud

---
+++ mysite/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/shanecheek/Desktop/PDX_Code/my_repos/gomoto_gcloud/mysite/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
