bee4ba7105564a2026003e2902ccfea55281e0b6
Shane Cheek <unitedideas@gmail.com>
2018-07-24 15:23:43 -0700

    removing unusable view

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -156,7 +156,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-
 STATIC_URL = 'https://storage.googleapis.com/lobosevents-static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
